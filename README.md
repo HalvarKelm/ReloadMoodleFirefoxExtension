c7d72aedeac0b41a5f33d343e98c5fca15298ef7
HalvarKelm <halvar.kelm@gmail.com>
2019-07-11 13:29:50 +0200

    changed for more general functionality and now with duplicating tab and closing it shortly after

--- README.md
+++ README.md
@@ -3,9 +3,5 @@ A extension, which reloads inactive moodle links every 29 mins to prevent the cu
 
 This extension will not close documents (if it happens, let me know) and will not reload the tab if it is currently being viewed at.
 
-HOW TO INSTALL:
-  1. URL: about:debugging
-  2. Click "Load Temporary Add-On" in the top right and navigate to the extracted folder and click on the "reloadmoodleextension.js"
-
 TODO:
   - if the only open tab is a document or is an active moodle tab open a tab in the background and close it again instantly
--- reloadmoodleextension.js
+++ reloadmoodleextension.js
@@ -1,18 +1,27 @@
+function remove(tab){
+    setTimeout(function(){
+        browser.tabs.remove(
+            tab.id
+        );
+    },1000);
+}
+
 //repeat every 29 minutes since moodle requires you to reload a tab every 30 mins stated here: https://docs.moodle.org/37/en/Session_handling
 setInterval(function(){
-
     //get all tabs
     browser.tabs.query({}).then((tabs) => {
-
         //iterate over tabs
         for (var tab of tabs) {
-
-            //TODO: if the only matching tab is a document create a new tab and close it instantly
-            //tab has to be inactive and the standard URL or another URL with .php but not .php/ so documents do not get reloaded
-            if(!tab.active && (tab.url == 'https://moodle.hs-mannheim.de/' || tab.url.match(new RegExp('(?:(?!\://moodle\.)(?:.|\n))*\://moodle\.(?:(?!\.php)(?:.|\n))*\.php[^/;]+')))){
-                browser.tabs.reload(
-                    tab.id
-                )
+            if(tab.url.match(new RegExp('(?:(?!\://moodle\.)(?:.|\n))*\://moodle\.'))){
+                //moodle tab found
+                var creating = browser.tabs.create({
+                    active: false,
+                    url: tab.url,
+                });
+                creating.then(
+                    remove
+                );
+                break;
             }
         }
     });
